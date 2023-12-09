1aaba1e9b00ae7cf0b5c8aedaa1cdc226e038924
Gustavo Costa <gustavocosta.miguel@estudante.ufjf.br>
2023-12-09 11:54:17 -0300

    Função de calcular pontuação dos jogadores

--- src/jogo.js
+++ src/jogo.js
@@ -1,6 +1,7 @@
 let vezDoJogador = 1;
 
 export function criarTabuleiro(jogador) {
+    // Função que cria o tabuleiro dos jogadores
     const tabela = document.getElementById(`tabela${jogador}`);
 
     if (!tabela) {
@@ -8,52 +9,56 @@ export function criarTabuleiro(jogador) {
         return;
     }
 
-    // Criar um elemento h2 para o texto acima da tabela
+    // Cria um elemento h2 para o texto acima da tabela
     const textoAcimaTabela = document.createElement('h2');
     textoAcimaTabela.textContent = `Tabuleiro do Jogador ${jogador}`;
 
-    // Inserir o elemento h2 antes da tabela
+    // Aplica estilos para centralizar o texto
+    textoAcimaTabela.style.textAlign = 'center';
+    textoAcimaTabela.style.marginTop = '0';
+    textoAcimaTabela.style.marginBottom = '10px';
+
+    // Insere o elemento h2 antes da tabela
     tabela.parentNode.insertBefore(textoAcimaTabela, tabela);
 
+    // Cria a tabela vazia
     for (let i = 0; i < 3; i++) {
         const linha = tabela.insertRow(i);
         for (let j = 0; j < 3; j++) {
             const celula = linha.insertCell(j);
-            celula.textContent = ''; // Inicialmente, sem valor
+            celula.textContent = '';
         }
     }
-}
 
-export function preencherTabela(numero, jogador, rowIndex, cellIndex) {
-    const tabela = document.getElementById(`tabela${jogador}`);
-    const linhaExistente = tabela.rows[rowIndex];
+    // Obtém a div existente para a pontuação
+    const pontuacao = document.getElementById(`pontuacaoJogador${jogador}`);
 
-    if (linhaExistente) {
-        const celula = linhaExistente.cells[cellIndex];
-
-        if (celula) {
-            celula.textContent = numero;
-        }
+    if (pontuacao) {
+        // Atualiza o conteúdo da pontuação
+        pontuacao.textContent = `Pontuação: 0`;
+        pontuacao.style.textAlign = 'center';
+        pontuacao.style.marginTop = '0';
+        pontuacao.style.marginBottom = '10px';
+    } else {
+        console.error(`Elemento com ID 'pontuacaoJogador${jogador}' não encontrado.`);
     }
 }
-
-export function realizarSorteio(jogador) {
-    const resultadoDoSorteio = rolarDado();
-    const elementoResultado = document.getElementById('resultadoDoSorteio');
-    elementoResultado.textContent = `Resultado do sorteio do dado: ${resultadoDoSorteio}. Jogador ${jogador}, clique em uma célula para inserir.`;
-
-    // Obtenha uma célula aleatória para preencher
-    const rowIndex = Math.floor(Math.random() * 3);
-    const cellIndex = Math.floor(Math.random() * 3);
-
-    preencherTabela(resultadoDoSorteio, jogador, rowIndex, cellIndex);
-
-    // Alterne para o próximo jogador
-    vezDoJogador = (vezDoJogador === 1) ? 2 : 1;
-}
-
 
 export function rolarDado() {
-    // Funcao que retorna um numero aleatorio entre 1 e 6
+    // Função que retorna um número aleatório entre 1 e 6 referente às faces do dado
     return Math.floor(Math.random() * 6) + 1;
+}
+
+
+function calcularPontuacao(tabela) {
+    let pontuacao = 0;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            const valorCelula = parseInt(tabela.rows[i].cells[j].textContent, 10) || 0;
+            pontuacao += valorCelula;
+        }
+    }
+
+    return pontuacao;
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { criarTabuleiro, realizarSorteio } from "./jogo.js";
+import { criarTabuleiro, realizarSorteio, sortearPrimeiroJogador } from "./jogo.js";
 
 // Função de inicialização
 function inicializarJogo() {
     // Inicialmente, criar a tabela 3x3
     criarTabuleiro(1);
     criarTabuleiro(2);
+
+    // Sortear a vez do primeiro jogador ao carregar a página (automaticamente)
+    document.getElementById('sorteioPrimeiroJogador').addEventListener('click', function () {
+        sortearPrimeiroJogador();
+        // Desative o botão após a execução
+        document.getElementById('sorteioPrimeiroJogador').disabled = true;
+        document.getElementById('sorteioPrimeiroJogador').style.display = 'none';
+    });
 
     // Associar eventos aos botões
     document.getElementById('rolarDadoJogador1').addEventListener('click', function () {
@@ -14,6 +22,9 @@ function inicializarJogo() {
     document.getElementById('rolarDadoJogador2').addEventListener('click', function () {
         realizarSorteio(2);
     });
+
+
+
 }
 
 // Chamar a função de inicialização quando o DOM estiver pronto
