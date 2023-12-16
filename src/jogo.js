d8c45644407459637308092dc73e45ebd09d8e77
danielabib04 <danielabib@outlook.com>
2023-12-16 20:06:15 -0300

    altera label por form

--- src/jogo.js
+++ src/jogo.js
@@ -1,8 +1,9 @@
+// funciona para rolar o dado e substituir o elemento da label
 function rolarDado (){
-    let dado = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("btnRolar").onclick = rolarDado;
-    document.getElementById("resultado").innerHTML = dado
+    const dado = Math.floor(Math.random() * 6) + 1;
     
+    document.getElementById("tabuleiro").innerHTML = dado
 }
+
 document.getElementById("btnRolar").onclick = rolarDado;
-document.getElementById()
+    
